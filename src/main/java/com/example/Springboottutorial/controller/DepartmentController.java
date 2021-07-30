a0e0dc6053b70a500ce28db3f48d91cf40c12e05
satyajeet <satyajeet@satyajeets-MacB.local>
2021-07-30 10:00:11 +0530

    pushing code with changes in service

--- src/main/java/com/example/Springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/example/Springboottutorial/controller/DepartmentController.java
@@ -34,6 +34,12 @@ public class DepartmentController {
       return "Department Deleted Successfully";
    }
 
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department) {
+        return departmentService.updateDepartment(departmentId,department);
+    }
+
 
 
 }
--- src/main/java/com/example/Springboottutorial/service/DepartmentService.java
+++ src/main/java/com/example/Springboottutorial/service/DepartmentService.java
@@ -11,4 +11,6 @@ public interface DepartmentService {
    public  Department fetchDepartmentById(Long departmentId);
 
    public  void deleteDepartmentById(Long departmentId);
+
+   public Department updateDepartment(Long departmentId, Department department);
 }
