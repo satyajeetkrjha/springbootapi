f7a006de210d2818a8ef19b6fc474d7b2b892420
satyajeet <satyajeet@satyajeets-MacB.local>
2021-07-30 13:28:31 +0530

    pushing code with changes

--- src/main/java/com/example/Springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/example/Springboottutorial/controller/DepartmentController.java
@@ -39,6 +39,10 @@ public class DepartmentController {
                                        @RequestBody Department department) {
         return departmentService.updateDepartment(departmentId,department);
     }
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+      return departmentService.fetchDepartmentByName(departmentName);
+    }
 
 
 
--- src/main/java/com/example/Springboottutorial/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/Springboottutorial/service/DepartmentServiceImpl.java
@@ -55,4 +55,9 @@ public class DepartmentServiceImpl implements  DepartmentService {
 
 
     }
+
+    @Override
+    public Department fetchDepartmentByName(String departmentName) {
+      return departmentRepository.findByDepartmentName(departmentName);
+    }
 }
